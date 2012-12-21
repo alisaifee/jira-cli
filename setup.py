0b5626ed356b203ec19ed23f3b179b82670206bc
Ali-Akber Saifee <ali@mig33global.com>
2012-12-21 09:11:55 +0800

    make versioning static

--- setup.py
+++ setup.py
@@ -8,18 +8,13 @@ import os
 import sys
 from setuptools import setup, find_packages, Command
 
-default_version="0.1"
-def get_version_from_tag():
-    is_tag = os.popen("git describe").read().strip()
-    if not is_tag:
-        return default_version
-    return is_tag
+version="0.2.7"
 
 setup(name='jira-cli',
      author="Ali-Akber Saifee",
      author_email="ali@indydevs.org",
      url="http://hg.indydevs.org/jira-cli",
-     version = get_version_from_tag(),
+     version = version,
      description = "command line utility for interacting with jira",
      long_description = open("README").read(),
      packages = find_packages(exclude=['ez_setup']),
