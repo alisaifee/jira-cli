08480af5a78ac228e2f797f0389ad8752eb29934
Ali-Akber Saifee <ali@mig33global.com>
2013-01-09 08:50:57 +0800

    added license information

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup.py for jira-cli
 """
 __author__ = "Ali-Akber Saifee"
 __email__ = "ali@mig33global.com"
+__copyright__ = "Copyright 2013, ProjectGoth"
 
 import os
 import sys
@@ -13,6 +14,7 @@ setup(name='jira-cli',
      author=__author__,
      author_email=__email__,
      url="http://github.com/mig33/jira-cli",
+     license="MIT",
      version = jiracli.__version__,
      description = "command line utility for interacting with jira",
      long_description = open("README.rst").read(),
