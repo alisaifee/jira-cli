e46c88013a73260403d947a711e358c20b7647a6
Ali-Akber Saifee <ali@indydevs.org>
2016-01-28 11:32:46 +0800

    Remove signed tagging for now

--- tag.sh
+++ tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 echo current version:$(python -c "import jiracli;print jiracli.__version__")
 read -p "new version:" new_version
-git tag -s ${new_version} -m "tagging version ${new_version}"
+git tag ${new_version} -m "tagging version ${new_version}"
 python setup.py build sdist bdist_egg upload
 
 
