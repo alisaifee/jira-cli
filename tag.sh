63d39fa2cfe13bca9c24337e689f6e232701e15b
Ali-Akber Saifee <ali@indydevs.org>
2017-07-30 15:44:14 +0800

    Fix syntax error in tagging script

--- tag.sh
+++ tag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-echo current version:$(python -c "import jiracli;print jiracli.__version__")
+echo current version:$(python -c "import jiracli;print(jiracli.__version__)")
 read -p "new version:" new_version
 git tag -s ${new_version} -m "tagging version ${new_version}"
 python setup.py build sdist bdist_egg upload
