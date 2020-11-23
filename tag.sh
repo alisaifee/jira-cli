5d8854ea433f9bde89c909a25eec69b0af58070d
Ali-Akber Saifee <ali@indydevs.org>
2020-11-22 20:52:27 -0800

    Update tagging script

--- tag.sh
+++ tag.sh
@@ -1,7 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
+rm -rf build dist
 echo current version:$(python -c "import jiracli;print(jiracli.__version__)")
 read -p "new version:" new_version
 git tag -s ${new_version} -m "tagging version ${new_version}"
-python setup.py build sdist bdist_egg upload
+python setup.py build sdist bdist_wheel
+twine upload dist/*
 
 
